3fce4de3473d6b4fe7ed482cafa8b675653ea53a
MRili123 <iliasssjb2004@gmail.com>
2026-04-16 13:14:02 +0100

    sfsfsfsf

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ app.get("/", (req, res) => {
   res.send(`Nombre de visites : ${count}`);
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
